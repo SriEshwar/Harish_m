eaa587d510b2d7c293fab570dd8e19b909485987
Harish12121 <harishmsece@gmail.com>
2024-04-23 14:49:37 +0530

    Express-Form Created

---
+++ ExpressJs/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000;
+
+// Middleware to parse form data
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/submit', (req, res) => {
+    const { name, phone, email, password } = req.body;
+    const userInfo = `Name: ${name}, Phone: ${phone}, Email: ${email}, Password: ${password}`;
+    res.send(userInfo);
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
