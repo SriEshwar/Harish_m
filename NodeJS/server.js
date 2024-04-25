ab90a1606d9008fbc467077357fcca01afc48d5c
Harish12121 <harishmsece@gmail.com>
2024-04-26 00:42:57 +0530

    NodeJS folder added

---
+++ NodeJS/server.js
@@ -0,0 +1,9 @@
+const os = require('os')
+const path = require('path')
+
+console.log(__dirname)
+console.log(__filename)
+
+//console.log(path.dirname(__filename))
+
+console.log(path.parse(__dirname))
