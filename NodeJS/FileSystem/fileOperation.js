6fbc7b2ad1d7b7c419f6496ecd3b42b39a4ecc22
Harish12121 <harishmsece@gmail.com>
2024-04-26 14:14:12 +0530

    File System operation Added

--- NodeJS/FileSystem/fileOperation.js
+++ NodeJS/FileSystem/fileOperation.js
@@ -14,5 +14,17 @@ fs.readFile(path.join(__dirname,'sample.txt'),'utf8', (err, data) => {
 
 fs.writeFile(path.join(__dirname,'sample2.txt'),'Hi, File write Operation.', (err) => {
     if (err) throw err;
-    console.log('write Completed');
+    console.log('write Completed')
+    fs.appendFile(path.join(__dirname,'sample2.txt'),'\n\n Thankyou.', (err) => {
+        if (err) throw err;
+        console.log('Append completed')
+        fs.rename(path.join(__dirname,'sample2.txt'), path.join(__dirname,'Greeting.txt'), (err) => {
+            if (err) throw err;
+            console.log('Rename Completed');
+          }); 
+      }); 
   }); 
+
+
+
+ 
