0648f3f31e77003120529e0a88f70427dc14aaae
Harish12121 <harishmsece@gmail.com>
2024-05-10 09:09:47 +0530

    mongoose file Added

--- Mongodb/mongooseConnection.js
+++ Mongodb/mongooseConnection.js
@@ -2,6 +2,8 @@ var mongoose = require('mongoose');
 
 var Schema = mongoose.Schema;
 
+async function mongooseConnect(){
+
 var skillSchema = new Schema({
     skillName: String,
     experience: Number,
@@ -16,19 +18,29 @@ var jobSeekerSchema = new Schema({
     skills: [skillSchema]
 });
 
+
 var JobSeeker = mongoose.model('JobSeeker', jobSeekerSchema);
 
 mongoose.connect("mongodb://127.0.0.1:27017/aspire");
 
-var jobseeker1 = new JobSeeker({
-    jobseekerName: "Hari",
-    age: 22,
-    emailId: "hari@test.com",
-    certified: true,
-    skills: [
-        { skillName: "java", experience: 1, proficiency: "beginner" },
-        { skillName: "angular", experience: 2, proficiency: "intermediate" }
-    ]
-});
+// var jobseeker1 = new JobSeeker({
+//     jobseekerName: "Hari",
+//     age: 22,
+//     emailId: "hari@test.com",
+//     certified: true,
+//     skills: [
+//         { skillName: "java", experience: 1, proficiency: "beginner" },
+//         { skillName: "angular", experience: 2, proficiency: "intermediate" }
+//     ]
+// });
 
-jobseeker1.save();
+// jobseeker1.save();
+
+const updateName = await JobSeeker.findOneAndUpdate({"jobseekerName": "Hari"},{ $set:{ "age": 25 }});
+
+
+const result = await JobSeeker.find({"skills.experience":{$lte:2}})
+result.map(record => console.log(record))
+}
+
+mongooseConnect();
